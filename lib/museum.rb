619d9232df24a0ff45aab977ac06276a22df51eb
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-07 10:05:15 -0700

    Add patron indexing by said interest hash method

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,13 +1,19 @@
 class Museum
 	attr_reader :name,
-				:exhibits
+				:exhibits,
+				:patrons
 	def initialize(name)
 		@name = name
 		@exhibits = []
+		@patrons = []
 	end
 	
 	def add_exhibit(exhibit)
 		@exhibits << exhibit
+	end
+
+	def admit(patron)
+		@patrons << patron
 	end
 
 	def recommend_exhibits(patron)
@@ -15,4 +21,30 @@ class Museum
 			patron.interests.include? exhibit.name
 		end
 	end
+
+	def patrons_by_exhibit_interest
+		breakdown = Hash.new
+
+		@exhibits.each do |exhibit|
+			@patrons.each do |patron|
+				breakdown[exhibit] ||= []
+				breakdown[exhibit] << patron unless !patron.interests.include? exhibit.name
+			end			
+		end
+		breakdown
+	end
+
 end
+
+# The short answer seems to be: a ||= b means,
+# if a is undefined then assign it the value of b, 
+#otherwise leave it alone.
+=begin
+
+@exhibits.flat_map do |exhibit|
+			@patrons.flat_map do |patron|
+				breakdown[exhibit] << patron if recommend_exhibits(patron)
+			end
+		end
+
+=end
