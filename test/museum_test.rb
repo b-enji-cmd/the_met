3a31bccc306f480d2faed71a1814d9f61cd09166
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-07 10:04:56 -0700

    Add indexing exhibits by interest

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -11,10 +11,9 @@ class MuseumTest < Minitest::Test
 		@dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
 		@imax = Exhibit.new({name: "IMAX",cost: 15})
 		@patron_1 = Patron.new("Bob", 20)
-		@patron_1.add_interest("Dead Sea Scrolls")
-		@patron_1.add_interest("Gems and Minerals")
 		@patron_2 = Patron.new("Sally", 20)
-		@patron_2.add_interest("IMAX")
+		@patron_3 = Patron.new("Johnny", 5)
+		@patron_3.add_interest("Dead Sea Scrolls")
 	end
 
 	def test_it_is
@@ -37,8 +36,49 @@ class MuseumTest < Minitest::Test
 		@dmns.add_exhibit(@gems_and_minerals)
 		@dmns.add_exhibit(@dead_sea_scrolls)
 		@dmns.add_exhibit(@imax)
-		
+
+		@patron_1.add_interest("Dead Sea Scrolls")
+		@patron_1.add_interest("Gems and Minerals")
+		@patron_2.add_interest("IMAX")
+
 		assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@patron_1)
 		assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
+	end
+
+	def test_it_can_order_patrons_by_exhibit
+		@dmns.add_exhibit(@gems_and_minerals)
+		@dmns.add_exhibit(@dead_sea_scrolls)
+		@dmns.add_exhibit(@imax)
+
+
+
+		@patron_1.add_interest("Gems and Minerals")
+		@patron_1.add_interest("Dead Sea Scrolls")
+
+		@patron_2.add_interest("Dead Sea Scrolls")
+
+		@patron_3.add_interest("Dead Sea Scrolls")
+
+		@dmns.admit(@patron_1)
+		@dmns.admit(@patron_2)
+		@dmns.admit(@patron_3)
+
+
+		actual = {
+			@gems_and_minerals => [@patron_1],
+			@dead_sea_scrolls => [@patron_1, @patron_2, @patron_3],
+			@imax => []
+		}
+		@dmns.patrons_by_exhibit_interest
+		assert_equal actual, @dmns.patrons_by_exhibit_interest
+	end
+
+	def test_it_can_admit_patrons
+		assert_equal [], @dmns.patrons
+		@dmns.admit(@patron_1)
+		@dmns.admit(@patron_2)
+		@dmns.admit(@patron_3)
+
+		assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
 	end
 end
