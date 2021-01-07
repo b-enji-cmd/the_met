be8aaf8972731d209f1d1f7b37a4c50185043669
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-07 09:19:27 -0700

    Add instance test for Museum

---
+++ test/museum_test.rb
@@ -0,0 +1,18 @@
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+require "minitest/autorun"
+require "pry"
+
+class MuseumTest < Minitest::Test
+	def setup
+		@dmns = Museum.new("Denver Museum of Nature and Science")
+		@gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+		@dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+		@imax = Exhibit.new({name: "IMAX",cost: 15})
+	end
+
+	def test_it_is
+		assert_instance_of Museum, @dmns
+	end
+end
