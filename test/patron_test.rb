931a13ac49e76eaac4b403ef6454969af082ba43
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-07 09:16:11 -0700

    Add interest tests

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -17,4 +17,14 @@ class PatronTest < Minitest::Test
 		assert_equal "Bob", @patron_1.name
 		assert_equal 20, @patron_1.spending_money
 	end
+
+	def test_it_can_have_interests
+		assert_equal [], @patron_1.interests
+	end
+
+	def test_it_can_add_interests
+		@patron_1.add_interest("Dead Sea Scrolls")
+		@patron_1.add_interest("Gems and Minerals")
+		assert_equal ["Dead Sea Scrolls","Gems and Minerals"], @patron_1.interests
+	end
 end
