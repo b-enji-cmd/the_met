f8e18e9a97b106e6074be2b32329021fce127eb5
Ben Fulton <benjamin.fulton99@gmail.com>
2021-01-07 10:56:13 -0700

    Add lottery draws and winners test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -2,6 +2,7 @@ require './lib/exhibit'
 require './lib/patron'
 require './lib/museum'
 require "minitest/autorun"
+require 'mocha/minitest'
 require "pry"
 
 class MuseumTest < Minitest::Test
@@ -104,5 +105,49 @@ class MuseumTest < Minitest::Test
 		#SALLY IS #<Patron:0x00007fb20227f8b0...>
 		#JOHNNY IS #<Patron:0x6666fb20114megan...>
 		assert_equal [patron_1, patron_3], @dmns.ticket_lottery_contestants(@dead_sea_scrolls)
+	end
+
+	def test_it_can_draw_a_winner
+		@dmns.add_exhibit(@gems_and_minerals)
+		@dmns.add_exhibit(@dead_sea_scrolls)
+		@dmns.add_exhibit(@imax)
+
+		patron_1 = Patron.new("Bob", 0)
+		patron_2 = Patron.new("Sally", 20)
+		patron_3 = Patron.new("Johnny", 5)
+
+		patron_1.add_interest("Gems and Minerals")
+		patron_1.add_interest("Dead Sea Scrolls")
+		patron_2.add_interest("Dead Sea Scrolls")
+		patron_3.add_interest("Dead Sea Scrolls")
+
+		@dmns.admit(patron_1)
+		@dmns.admit(patron_2)
+		@dmns.admit(patron_3)
+
+		@dmns.draw_lottery_winner(@dead_sea_scrolls)
+		@dmns.stub(:draw_lottery_winner).returns(patron_1)
+
+	end
+
+	def test_it_can_announce_lottery
+		@dmns.add_exhibit(@gems_and_minerals)
+		@dmns.add_exhibit(@dead_sea_scrolls)
+		@dmns.add_exhibit(@imax)
+
+		patron_1 = Patron.new("Bob", 0)
+		patron_2 = Patron.new("Sally", 20)
+		patron_3 = Patron.new("Johnny", 5)
+
+		patron_1.add_interest("Gems and Minerals")
+		patron_1.add_interest("Dead Sea Scrolls")
+		patron_2.add_interest("Dead Sea Scrolls")
+		patron_3.add_interest("Dead Sea Scrolls")
+
+		@dmns.admit(patron_1)
+		@dmns.admit(patron_2)
+		@dmns.admit(patron_3)
+
+
 	end
 end
